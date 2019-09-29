efd17dba79422a91cd2fb164616ba3948eceaa46
Pam Cairo <pcairo_90@outlook.com>
2019-09-29 23:11:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,9 +3,6 @@ def introduction(name)
 end
 
 
-# thing is NOT to "complete labs," the important thing is to learn the skills you need
-# in order to complete the rest of this course and be ready for the next course!
-
 
 
 
